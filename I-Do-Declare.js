17466a42d1f26d5993676a80b4ed671c521277ab
Nathan Haberman <na.haberman@gmail.com>
2017-08-18 13:53:24 -0500

    Finished Assignment

---
+++ I-Do-Declare.js
@@ -0,0 +1,23 @@
+var num1 = 10;
+console.log("First Number:",num1);
+var num2 = 1.5;
+console.log("Second Number:",num2);
+var num3 = -15;
+console.log("Third Number:",num3);
+var num4 = -10.5;
+console.log("Fourth Number:",num4);
+
+var string1 = "Hello World";
+console.log("String 1:",string1)
+var string2 = "Can be numbers too! 1,2,3!";
+console.log("String 2:",string2)
+var string3 = "Goodbye World";
+console.log("String 3:",string3)
+
+var boolean1 = true;
+console.log("Can Booleans be true or false?",boolean1);
+var boolean2 = false;
+console.log("Can Booleans be more than true or false?",boolean2);
+
+var undef = undefined;
+console.log("This variable is undefined, see:",undef);
